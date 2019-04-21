e4ad90079a59ed677ab40aaacf74a497800fe09c
Uros Trstenjak <uros.trstenjak@gmail.com>
2019-04-21 10:26:25 +0200

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Test
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
